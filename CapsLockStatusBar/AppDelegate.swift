ca90e9093f1232a02945fe18a16a87fbdd768f63
Hykilpikonna <me@hydev.org>
2020-09-03 17:10:18 -0400

    [+] Create NSStatusItem

--- CapsLockStatusBar/AppDelegate.swift
+++ CapsLockStatusBar/AppDelegate.swift
@@ -10,8 +10,18 @@ import Cocoa
 import SwiftUI
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+    // Status bar item
+    var statusItem: NSStatusItem?
 
+    // Set status bar
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+
+        // Create status item
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     }
     }
 }
