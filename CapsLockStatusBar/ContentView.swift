7e96a81fda7b5c436492af18434d758836c511a1
Hykilpikonna <me@hydev.org>
2020-09-03 16:08:03 -0400

    Initial Commit

---
+++ CapsLockStatusBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CapsLockStatusBar
+//
+//  Created by Hykilpikonna on 9/3/20.
+//  Copyright © 2020 Hykilpikonna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
