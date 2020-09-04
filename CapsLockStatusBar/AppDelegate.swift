4662622618f4cbdceb9d8394b7c4fa0c5bdaaeba
Hykilpikonna <me@hydev.org>
2020-09-03 22:24:33 -0400

    [O] Create status item on init

--- CapsLockStatusBar/AppDelegate.swift
+++ CapsLockStatusBar/AppDelegate.swift
@@ -13,7 +13,7 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate
 {
     // Status bar item
-    var statusItem: NSStatusItem?
+    var statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     
     // Menu link
     @IBOutlet weak var menu: NSMenu?
@@ -28,23 +28,20 @@ class AppDelegate: NSObject, NSApplicationDelegate
         
         // Add capslocks event handler
         NSEvent.addGlobalMonitorForEvents(matching: .flagsChanged, handler: flagsChanged)
-
-        // Create status item
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         
         // Image
-        statusItem?.button?.image = images[false]
+        statusItem.button?.image = images[false]
         
         // Open application menu on click (TODO: Simplify?)
         if let menu = menu {
-            statusItem?.menu = menu
+            statusItem.menu = menu
         }
     }
     
     // Event handler to check if capslock is enabled
     func flagsChanged(with event: NSEvent)
     {
-        statusItem?.button?.image = images[event.modifierFlags.intersection(.deviceIndependentFlagsMask).contains(.capsLock)]
+        statusItem.button?.image = images[event.modifierFlags.intersection(.deviceIndependentFlagsMask).contains(.capsLock)]
     }
 }
 
