f8b5047a22cebbe03135178975beb11b0f9a7930
Hykilpikonna <me@hydev.org>
2020-09-04 22:58:27 -0400

    [+] Finish icon resolution conversion script

--- Icons/convert.py
+++ Icons/convert.py
@@ -3,6 +3,13 @@ import os
 from os import listdir
 from os.path import isfile
 
+baseRes = 16
+reses = [baseRes, baseRes * 2, baseRes * 3]
+
 for f in listdir():
     if isfile(f) and f.endswith("png"):
-        os.system("echo " + f)
+        name = f[:-4]
+        names = list(map(lambda x: name + "@{}x.png".format(x), [1, 2, 3]))
+
+        for i in range(3):
+            os.system("convert {} -resize {}x{} {}".format(f, reses[i], reses[i], names[i]))
